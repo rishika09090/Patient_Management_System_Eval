3c3c8afdcdd877d16cded1fc09f8117dc7adac34
Rishika Kumari <rishika09090@gmail.com>
2025-03-01 17:47:34 +0530

    completedd

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const connectDB = require("./config/db");
 const redisClient = require("./config/redis");
 const authRoutes = require("./routes/authRoutes");
+const patientRoutes = require("./routes/patient.routes");
+const doctorRoutes = require("./routes/doctor.routes");
+const adminRoutes = require("./routes/admin.routes");
 const logger = require("./middlewares/logger");
 // const errorHandler = ;
 
@@ -15,6 +18,9 @@ app.use(logger);
 connectDB();
 
 app.use("/auth", authRoutes);
+app.use("/patient", patientRoutes);
+app.use("/admin", adminRoutes);
+app.use("/doctor", doctorRoutes);
 app.use(require("./middlewares/errorHandler"));
 
 const PORT = process.env.PORT || 5000;
